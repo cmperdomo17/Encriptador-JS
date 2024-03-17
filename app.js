80ba8c83552c7c387e3a6af1cdf85a6e28d7db63
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-03-16 23:48:00 -0500

    mensaje de error lowercase

--- app.js
+++ app.js
@@ -15,17 +15,25 @@ function setPlaceholder() {
 function encriptar() {
     let texto = document.getElementById("textbox").value;
     let noTextFound = document.querySelector(".no_text_found");
+    let span = document.querySelector("#tooltiptext");
+    let svgPath = document.querySelector(".tooltip svg path");
     let textFound = document.querySelector(".result_layout");
 
     const minusculas = /^[a-z\s]+$/;
     if (!minusculas.test(texto)) {
-        alert("Solo se permiten letras minúsculas y sin acentos");
+        span.textContent = "No se permiten mayúsculas ni acentos. Intente de nuevo."
+        span.style.color = "red";
+        svgPath.style.fill = "red";
         return;
+    } else {
+        span.textContent = "Solo letras minúsculas y sin acentos";
+        span.style.color = "";
+        svgPath.style.fill = "";
     }
 
     if (texto === "") {
         noTextFound.style.display = "block";
-        textFound.style.display = "none";
+        textFound.style.display = "none";   
     } else {
         noTextFound.style.display = "none";
         let textoEncriptado = texto.replace(/e/g, "enter")
@@ -42,12 +50,6 @@ function desencriptar() {
     let texto = document.getElementById("textbox").value;
     let noTextFound = document.querySelector(".no_text_found");
     let textFound = document.querySelector(".result_layout");
-
-    const minusculas = /^[a-z\s]+$/;
-    if (!minusculas.test(texto)) {
-        alert("Solo se permiten letras minúsculas y sin acentos");
-        return;
-    }
 
     if (texto === "") {
         noTextFound.style.display = "block";
